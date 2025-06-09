d799b7c9418f920b8c37dcb7bb77acaadd817329
agucitri <86527530+agucitri@users.noreply.github.com>
2025-06-09 17:23:53 -0300

    Add files via upload

--- proyectoCoder-functions.sql
+++ proyectoCoder-functions.sql
@@ -2,11 +2,11 @@
 USE proyectoCoder;
  
 
--- (1) function q_ventas_prod_categ
-DROP FUNCTION IF EXISTS ventas_prod_categ;
+-- (1) function f_ventas_prod_categ
+DROP FUNCTION IF EXISTS f_ventas_prod_categ;
 
 DELIMITER $$
-CREATE FUNCTION ventas_prod_categ (p_id_prod_categ INT)
+CREATE FUNCTION f_ventas_prod_categ (p_id_prod_categ INT)
 RETURNS DECIMAL (12, 2)
 DETERMINISTIC
 BEGIN
@@ -14,8 +14,8 @@ BEGIN
     
     SELECT SUM(od.quantity * p.price)
 	INTO monto
-    FROM order_details od
-    INNER JOIN products p USING (id_product)
+    FROM ORDER_DETAILS AS od
+    INNER JOIN PRODUCTS AS p USING (id_product)
     WHERE p.id_prod_categ = p_id_prod_categ;
     
     RETURN IFNULL(monto, 0.00);
@@ -23,16 +23,16 @@ END
 $$
 DELIMITER ;
 
-SELECT category, ventas_prod_categ(id_prod_categ) AS total_ventas
-FROM prod_categ
+SELECT category, f_ventas_prod_categ(id_prod_categ) AS total_ventas
+FROM PROD_CATEG
 ORDER BY total_ventas DESC;
 
 
--- (2) function customer_q_ventas
-DROP FUNCTION IF EXISTS customer_q_ventas;
+-- (2) function f_customer_q_ventas
+DROP FUNCTION IF EXISTS f_customer_q_ventas;
 
 DELIMITER $$
-CREATE FUNCTION customer_q_ventas (p_id_customer INT)
+CREATE FUNCTION f_customer_q_ventas (p_id_customer INT)
 RETURNS INT
 DETERMINISTIC
 BEGIN
@@ -40,8 +40,8 @@ BEGIN
     
     SELECT COUNT(id_order)
     INTO cantidad
-    FROM orders o
-    INNER JOIN customers c USING (id_customer)
+    FROM ORDERS AS o
+    INNER JOIN CUSTOMERS AS c USING (id_customer)
     WHERE c.id_customer = p_id_customer;
     
     RETURN IFNULL(cantidad, 0);
@@ -49,7 +49,8 @@ END
 $$
 DELIMITER ;
 
-SELECT id_customer, first_name, last_name, customer_q_ventas(id_customer) AS compras_realizadas
+SELECT id_customer, first_name, last_name, f_customer_q_ventas(id_customer) AS compras_realizadas
 FROM customers
-ORDER BY compras_realizadas DESC;
+ORDER BY compras_realizadas DESC
+LIMIT 5;
 
